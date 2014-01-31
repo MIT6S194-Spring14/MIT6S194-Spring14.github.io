a92ba440e013328cdeb48294581a20e950d36ae2
Luke Faraone <lfaraone@csail.mit.edu>
2014-01-31 10:57:30 -0500

    Switch to subdirectory path

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/6.s194/www/"
+http_images_path = "/6.s194/www/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/6.s194/www/fonts"
+css_dir = "public/6.s194/www/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
